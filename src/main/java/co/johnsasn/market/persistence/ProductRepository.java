4ca5aae7925d83ffd5cee58a7e2201f10a42383c
John Santa <andersonsanta3@gmail.com>
2021-06-19 21:46:36 -0500

    Create product repository

--- src/main/java/co/johnsasn/market/persistence/ProductRepository.java
+++ src/main/java/co/johnsasn/market/persistence/ProductRepository.java
@@ -2,14 +2,37 @@ package co.johnsasn.market.persistence;
 
 import co.johnsasn.market.persistence.crud.ProductCrudRepository;
 import co.johnsasn.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductRepository {
 
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll(){
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> getByCategory(int categoryId){
+        return productCrudRepository.findByCategoryIdOrderByNameAsc(categoryId);
+    }
+
+    public Optional<List<Product>> getScarse(int stock, boolean status){
+        return productCrudRepository.findByStockLessThanAndStatus(stock, status);
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productCrudRepository.findById(productId);
+    }
+
+    public Product save(Product product){
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int productId){
+        productCrudRepository.deleteById(productId);
     }
 }
--- src/main/java/co/johnsasn/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/co/johnsasn/market/persistence/crud/ProductCrudRepository.java
@@ -3,7 +3,12 @@ package co.johnsasn.market.persistence.crud;
 import co.johnsasn.market.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
 
+    List<Product> findByCategoryIdOrderByNameAsc(int categoryId);
 
+    Optional<List<Product>> findByStockLessThanAndStatus(int stock, boolean status);
 }
